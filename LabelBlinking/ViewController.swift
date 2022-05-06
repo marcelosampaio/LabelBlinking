22d13c75c1a8182999cc54285fc33d4f80d35d07
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-05-06 15:58:12 -0300

    initial commit

--- LabelBlinking/ViewController.swift
+++ LabelBlinking/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import BlinkingRandomLabel
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    var isBlinking = false
+    
+    
+    // MARK: - Outlets
+    
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let blinkingLabel = BlinkingRandomLabel(frame: CGRect(x: 10, y: 20, width: 200, height: 30))
+        // Setup the BlinkingRandomLabel
+        blinkingLabel.text = "This label blinks random colors"
+        blinkingLabel.font = UIFont.systemFont(ofSize: 20) // UIFont.systemFontOfSize(20)
+        view.addSubview(blinkingLabel)
+        blinkingLabel.startBlinking()
+        isBlinking = true
+        
+        
     }
 
 
